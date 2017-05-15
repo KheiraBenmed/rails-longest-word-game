be9e620dbb4c0d65f9ba22a61361723d6d8dfdbe
Kheira Benmeridja <kheira.benmeridja@gmail.com>
2017-05-15 17:03:22 +0200

    exo 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'longestword/game' => 'longestword#game'
+  get 'longestword/score' => 'longestword#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
